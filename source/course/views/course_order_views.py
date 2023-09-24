c533b64434f9dff8cf89aabd80ce8e4274b20f3a
nooruzbai <nooruzbay@gmail.com>
2023-09-24 14:37:21 +0600

    Eddited buying course functionality in course_order_views.py

--- source/course/views/course_order_views.py
+++ source/course/views/course_order_views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.templatetags.rest_framework import data
 
 from course.models import Order
 from course.serializers import OrderSerializer
@@ -12,14 +13,14 @@ class CourseOrderView(CreateAPIView):
     queryset = Order.objects.all()
     permission_classes = (IsAuthenticated,)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({'Message': 'You have successfully bought the course'}, status=status.HTTP_201_CREATED, headers=headers)
+
     def perform_create(self, serializer):
         current_user = self.request.user
         order = serializer.save(user_id=current_user.id)
-        return Response({
-            "id": order.id,
-            "name": order.course.name,
-            "status": "Created"
-
-        }, status=status.HTTP_201_CREATED, content_type='application/json')
-
-
+        return order
